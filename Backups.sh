dd200dd37aa0f19351d6e9b7b19068aad95cf6d1
Salman Bobat <salman.9515@outlook.com>
2023-07-18 16:17:21 -0500

    Added the script file

---
+++ Backups.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Enter your drive location(s) here that need to be backed up.
+#For Ubuntu Server, drive directory locations are located in /media/.
+source_dirs=(
+  "/media/E_Drive_1"
+  "/media/E_Drive_2"
+  "/media/Vids"
+)
+
+#Removes previously archived files.
+rm /media/Backup_Drive/*.tar.gz
+
+#Enter the drive location where you want to place the backups.
+backup_dir="/media/Backup_Drive"
+
+#timestamp=$(date +%Y%m%d%H%M%S)
+
+# Compress and backup each source directory
+for dir in "${source_dirs[@]}"; do
+  base_dir=$(basename "$dir")
+  #archive_file="$backup_dir/$base_dir-$timestamp.tar.gz"
+  archive_file="$backup_dir/$base_dir.tar.gz"
+
+  # Create the backup archive
+  tar -I pigz -cf "$archive_file" "$dir"
+
+  # Verify the backup archive
+  if [ -f "$archive_file" ]; then
+    echo "Backup created: $archive_file"
+  else
+    echo "Backup failed: $archive_file"
+  fi
+done
